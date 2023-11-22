8151407b0f0c8ccec82854551c1f4dfdde4040b8
Jan Go <janemgo86@gmail.com>
2023-11-23 07:56:50 +0800

    move purple box to index

--- pages/header.js
+++ pages/header.js
@@ -4,21 +4,13 @@ export default function PriceHeader(params) {
   return (
     <>
       <Center>
-        <Box
-          bg="purple.500"
-          color="white"
-          h={"397"}
-          w={"100%"}
-          textAlign={"center"}
-        >
-          <Box>
-            <Text fontSize={"6xl"} fontWeight={"800"}>
-              Simple pricing for your business
-            </Text>
-            <Text opacity="84%">
-              Plans that are carefully crafted to suit your business
-            </Text>
-          </Box>
+        <Box>
+          <Text fontSize={"6xl"} fontWeight={"800"}>
+            Simple pricing for your business
+          </Text>
+          <Text opacity="84%">
+            Plans that are carefully crafted to suit your business
+          </Text>
         </Box>
       </Center>
     </>
--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,22 @@ import Link from "next/link";
 import PriceCard from "./prices";
 import PriceHeader from "./header";
 import PriceFeatures from "./features";
+import { Box } from "@chakra-ui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <PriceHeader />
+      <Box
+        bg="purple.500"
+        color="white"
+        h={"397"}
+        w={"100%"}
+        textAlign={"center"}
+      >
+        <PriceHeader />
+      </Box>
       <PriceCard />
       <PriceFeatures />
     </>
